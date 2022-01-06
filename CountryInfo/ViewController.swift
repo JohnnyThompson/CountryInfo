6804630b101c6c4f901a3e211ab99cd6580c5614
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2022-01-06 12:57:32 +0300

    Initial Commit

---
+++ CountryInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryInfo
+//
+//  Created by Евгений Карпов on 06.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
